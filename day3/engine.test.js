2c38e6d2a297f212bf37acacbdecde17c3028b51
David Lucas <davidalucas92@gmail.com>
2023-12-30 17:46:10 -0500

    Adds test with example data

--- day3/engine.test.js
+++ day3/engine.test.js
@@ -1,3 +1,20 @@
 import { expect, test } from "vitest";
 import { createReadStream } from "fs";
 import { createInterface } from "readline";
+import { addPartNumbers } from "./engine";
+
+test("addPartNumbers adds part numbers in example data", () => {
+  const data = [
+    "467..114..",
+    "...*......",
+    "..35..633.",
+    "......#...",
+    "617*......",
+    ".....+.58.",
+    "..592.....",
+    "......755.",
+    "...$.*....",
+    ".664.598..",
+  ];
+  expect(addPartNumbers(data)).toBe(4361);
+});
