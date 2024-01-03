3e861a025bf12451a83ef2f57de61586904a9dc9
David Lucas <davidalucas92@gmail.com>
2024-01-02 19:42:50 -0500

    Implements the algorithm and tests for Day 4 Part 2
    The test is running pretty slowly... might want to try improving the runtime perf a bit

--- day4/scratchcards.js
+++ day4/scratchcards.js
@@ -22,8 +22,6 @@ export function calculateWinnings(data) {
         .map((s) => parseInt(s))
         .filter((n) => !isNaN(n));
 
-      console.log(foundNumbers);
-
       for (const num of foundNumbers) {
         if (winningNumbers.find((n) => n === num)) {
           matches++;
@@ -33,4 +31,73 @@ export function calculateWinnings(data) {
       return matches === -1 ? 0 : 2 ** matches;
     })
     .reduce((a, b) => a + b);
+}
+
+/**
+ * Checks the current scratch card and provides the total number of matches
+ * @param {string[]} dataArr The raw string data array provided to us
+ * @param {int} idx The current index being analyzed
+ * @returns {int} The total number of matches found on the current line
+ */
+function sumMatches(dataArr, idx) {
+  let matches = 0;
+
+  const lineSplit = dataArr[idx].split("|");
+  const winningNumbers = lineSplit[0]
+    .split(":")[1]
+    .trim()
+    .split(" ")
+    .map((s) => parseInt(s))
+    .filter((n) => !isNaN(n));
+
+  const foundNumbers = lineSplit[1]
+    .trim()
+    .split(" ")
+    .map((s) => parseInt(s))
+    .filter((n) => !isNaN(n));
+
+  for (const num of foundNumbers) {
+    if (winningNumbers.find((n) => n === num)) {
+      matches++;
+    }
+  }
+
+  return matches;
+}
+
+/**
+ * Performs the recursion needed for the Day 4 Part 2 algorithm.
+ * @param {string[]} dataArr The raw data array.
+ * @param {int[]} totalArr The array keeping the tally of all of the cards.
+ * @param {int} idx The index we're currently on.
+ * @returns Nothing; returns when no more matches are found.
+ */
+function recurCalcTotal(dataArr, totalArr, idx) {
+  // termination condition
+  const matches = sumMatches(dataArr, idx);
+  if (matches === 0) {
+    return;
+  }
+
+  for (let i = 1; i <= matches; i++) {
+    //pre-recursion
+    totalArr[idx + 1]++;
+    //recurse
+    recurCalcTotal(dataArr, totalArr, idx + i);
+  }
+}
+
+/**
+ * Performs the algorithm for the Day 4 Part 2 challenge
+ * @param {string[]} data The raw data needing to be parsed
+ * @returns The total number of scratch cards
+ */
+export function calculateTotalCards(data) {
+  let totals = new Array(data.length).fill(1);
+
+  for (let i = 0; i < data.length - 1; i++) {
+    recurCalcTotal(data, totals, i);
+  }
+
+  return totals.reduce((a, b) => a + b);
 }
--- day4/scratchcards.test.js
+++ day4/scratchcards.test.js
@@ -1,7 +1,7 @@
 import { expect, test } from "vitest";
 import { createReadStream } from "fs";
 import { createInterface } from "readline";
-import { calculateWinnings } from "./scratchcards";
+import { calculateTotalCards, calculateWinnings } from "./scratchcards";
 
 test("calculateWinnings calculates correct sum for sample data", () => {
   const data = [
@@ -34,4 +34,37 @@ test("calculateWinnings calculates correct sum for real data", async () => {
   });
   const expected = 23235;
   expect(calculateWinnings(data)).toBe(expected);
+});
+
+test("calculateTotalCards calculates correct sum for sample data", () => {
+  const data = [
+    "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+    "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+    "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+    "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+    "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+    "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+  ];
+  const expected = 30;
+  expect(calculateTotalCards(data)).toBe(expected);
+});
+
+test("calculateTotalCards calculates correct sum for real data", async () => {
+  let data = [];
+
+  const readInterface = createInterface({
+    input: createReadStream("./day4/data.txt"),
+    output: process.stdout,
+    console: false,
+  });
+
+  await new Promise((resolve) => {
+    readInterface
+      .on("line", function (line) {
+        data.push(line);
+      })
+      .on("close", resolve);
+  });
+  const expected = 5920640;
+  expect(calculateTotalCards(data)).toBe(expected);
 });
