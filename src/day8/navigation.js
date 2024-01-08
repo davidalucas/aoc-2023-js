805bfeb0541ac8f981e85af9f5c0eaf64eace183
David Lucas <davidalucas92@gmail.com>
2024-01-08 10:56:46 -0500

    Adds new parseNavMap function
    Includes unit test to validate functionality

--- src/day8/navigation.js
+++ src/day8/navigation.js
@@ -1,3 +1,6 @@
+import { createReadStream } from "fs";
+import { createInterface } from "readline";
+
 /** @typedef {{left: string, right: string}} BinaryNode */
 /** @typedef {{directions: string, path: Record<string, BinaryNode>}} NavigationMap */
 
@@ -22,4 +25,41 @@ export function parseNode(data) {
     id: splitData[0],
     node: { left: lrSplitData[0], right: lrSplitData[1] },
   };
+}
+
+/**
+ * Parses a file at a given path, constructing a new NavigationMap object from the
+ * data within.
+ * @param {string} path The path to the file to parse
+ * @returns {Promise<NavigationMap>}
+ */
+export async function parseNavMap(path) {
+  /** @type {NavigationMap} */
+  const navMap = { directions: "", path: {} };
+  const readInterface = createInterface({
+    input: createReadStream(path),
+    output: process.stdout,
+    terminal: false,
+  });
+
+  await new Promise((resolve, reject) => {
+    readInterface
+      .on("line", (line) => {
+        if (line == "") {
+          return;
+        } else if (navMap.directions == "") {
+          navMap.directions = line;
+          return;
+        }
+        const nodeData = parseNode(line);
+        if (navMap.path[nodeData.id]) {
+          reject(
+            `Tried to overwrite value of path ${nodeData.id} with new data`,
+          );
+        }
+        navMap.path[nodeData.id] = nodeData.node;
+      })
+      .on("close", resolve);
+  });
+  return navMap;
 }
--- src/day8/navigation.test.js
+++ src/day8/navigation.test.js
@@ -1,9 +1,23 @@
 import { test, expect } from "vitest";
-import { parseNode } from "./navigation";
+import { parseNavMap, parseNode } from "./navigation";
 
 test("parseNode returns expected data", () => {
   /** @type {{id: string, node: import("./navigation").BinaryNode}} */
   let expected = { id: "AAA", node: { left: "BBB", right: "CCC" } };
   let actual = parseNode("AAA = (BBB, CCC)");
+  expect(actual).toStrictEqual(expected);
+});
+
+test("parseNavMap constructs expected NavigationMap object", async () => {
+  /** @type {import("./navigation").NavigationMap} */
+  const expected = {
+    directions: "LLR",
+    path: {
+      AAA: { left: "BBB", right: "BBB" },
+      BBB: { left: "AAA", right: "ZZZ" },
+      ZZZ: { left: "ZZZ", right: "ZZZ" },
+    },
+  };
+  const actual = await parseNavMap("./src/day8/example2.txt");
   expect(actual).toStrictEqual(expected);
 });
