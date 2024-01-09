e92f2cdb02658c4078eb41eb70530444fae888a9
David Lucas <davidalucas92@gmail.com>
2024-01-08 20:19:44 -0500

    Implements the predictPrevious and sumAllPrevPredictions functions
    Includes tests for both

--- src/day9/oasis.js
+++ src/day9/oasis.js
@@ -32,12 +32,33 @@ export function predictNext(sensorReadings) {
 }
 
 /**
+ * Recursively analyzes sensor data and returns the predicted previous value
+ * @param {number[]} sensorReadings The sensor readings to use for the prediction
+ * @returns {number}
+ */
+export function predictPrevious(sensorReadings) {
+  // termination condition
+  if (sensorReadings.every((r) => r == 0)) {
+    return 0;
+  }
+  // pre-recursion
+  const reducedReadings = new Array(sensorReadings.length - 1);
+  for (let i = 0; i < reducedReadings.length; i++) {
+    reducedReadings[i] = sensorReadings[i + 1] - sensorReadings[i];
+  }
+  // recursion
+  const prevVal = predictPrevious(reducedReadings);
+  // post-recursion
+  return sensorReadings[0] - prevVal;
+}
+
+/**
  * Performs the summation described in the Day 9 Part 1 problem using data
  * found in the file at the specified path location
  * @param {string} path The path to the raw data file
  * @returns {Promise<number>}
  */
-export async function sumAllPredictions(path) {
+export async function sumAllNextPredictions(path) {
   let sum = 0;
   const readInterface = createInterface({
     input: createReadStream(path),
@@ -49,6 +70,30 @@ export async function sumAllPredictions(path) {
     readInterface
       .on("line", (line) => {
         sum += predictNext(parseSensorData(line));
+      })
+      .on("close", resolve);
+  });
+  return sum;
+}
+
+/**
+ * Performs the summation described in the Day 9 Part 2 problem using data
+ * found in the file at the specified path location
+ * @param {string} path The path to the raw data file
+ * @returns {Promise<number>}
+ */
+export async function sumAllPrevPredictions(path) {
+  let sum = 0;
+  const readInterface = createInterface({
+    input: createReadStream(path),
+    output: process.stdout,
+    terminal: false,
+  });
+
+  await new Promise((resolve, reject) => {
+    readInterface
+      .on("line", (line) => {
+        sum += predictPrevious(parseSensorData(line));
       })
       .on("close", resolve);
   });
--- src/day9/oasis.test.js
+++ src/day9/oasis.test.js
@@ -1,21 +1,30 @@
 import { test, expect } from "vitest";
-import { parseSensorData, predictNext, sumAllPredictions } from "./oasis";
+import {
+  parseSensorData,
+  predictNext,
+  predictPrevious,
+  sumAllNextPredictions,
+  sumAllPrevPredictions,
+} from "./oasis";
 
 const sensorTestData = [
   {
     data: "0 3 6 9 12 15",
     expectedNumArray: [0, 3, 6, 9, 12, 15],
     expectedNextVal: 18,
+    expectedPrevVal: -3,
   },
   {
     data: "1 3 6 10 15 21",
     expectedNumArray: [1, 3, 6, 10, 15, 21],
     expectedNextVal: 28,
+    expectedPrevVal: 0,
   },
   {
     data: "10 13 16 21 30 45",
     expectedNumArray: [10, 13, 16, 21, 30, 45],
     expectedNextVal: 68,
+    expectedPrevVal: 5,
   },
 ];
 sensorTestData.forEach(({ data, expectedNumArray: expected }) => {
@@ -34,14 +43,35 @@ sensorTestData.forEach(
   },
 );
 
-test("sumAllPredictions returns expected result for example data", async () => {
+sensorTestData.forEach(
+  ({ expectedNumArray: data, expectedPrevVal: expected }) => {
+    test(`predictPrevious returns ${expected} for sequence [${data}]`, () => {
+      const actual = predictPrevious(data);
+      expect(actual).toBe(expected);
+    });
+  },
+);
+
+test("sumAllNextPredictions returns expected result for example data", async () => {
   const expected = 114;
-  const actual = await sumAllPredictions("./src/day9/example.txt");
+  const actual = await sumAllNextPredictions("./src/day9/example.txt");
   expect(actual).toBe(expected);
 });
 
-test("sumAllPredictions returns expected result for real data", async () => {
+test("sumAllNextPredictions returns expected result for real data", async () => {
   const expected = 1772145754;
-  const actual = await sumAllPredictions("./src/day9/data.txt");
+  const actual = await sumAllNextPredictions("./src/day9/data.txt");
+  expect(actual).toBe(expected);
+});
+
+test("sumAllPrevPredictions returns expected result for example data", async () => {
+  const expected = 2;
+  const actual = await sumAllPrevPredictions("./src/day9/example.txt");
+  expect(actual).toBe(expected);
+});
+
+test("sumAllPrevPredictions returns expected result for real data", async () => {
+  const expected = 867;
+  const actual = await sumAllPrevPredictions("./src/day9/data.txt");
   expect(actual).toBe(expected);
 });
